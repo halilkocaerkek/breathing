856b1ca6d2003853006662618e3a859a7a37ae68
Halil Kocaerkek <halil.kocaerkek@gmail.com>
2017-04-25 20:47:02 +0300

    initial commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,91 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $interval, Stages, $ionicPlatform) {
+    var h = window.screen.height - 300;
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    var stages;
+    var max;
+    var min;
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    var timer;
+
+    stages = Stages.all();
+    max = Stages.count();
+    min = 0;
+
+    init();
+
+    function init() {
+        $scope.gauge = null;
+        $scope.gauge = {
+            value: 0,
+            minValue: 0,
+            maxValue: Stages.count(),
+            width: h,
+            height: h,
+            dataTitle: "Nefes Al",
+            majorTics: Stages.majorTics(),
+            highlights: Stages.highlights()
+        };
+    };
+
+    $scope.start = function() {
+        // Don't start a new fight if we are already fighting
+        if (angular.isDefined(timer)) return;
+        init();
+        //vm.play('Tick');
+        timer = $interval(function() {
+            var v = $scope.gauge.value;
+            v++;
+            if (v > max) {
+                v = min;
+            }
+            $scope.gauge.value = v;
+            stages = Stages.all();
+            for (i = 0; i < stages.length; i++) {
+                if (v > stages[i].start && v < stages[i].end)
+                    $scope.gauge.dataTitle = stages[i].name;
+
+            }
+        }, 100);
+    };
+
+    $scope.stop = function() {
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        }
+        $scope.gauge.value = 0;
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatsCtrl', function($scope, Stages) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //   var a = 1;
+    //   $scope.stages = Stages.all();
+    //});
+
+
+    $scope.remove = function(stage) {
+        Stages.remove(stage);
+    };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Stages) {
+    $scope.stage = Stages.get($stateParams.chatId);
+
+    $scope.$watch('stage', function() {
+        Stages.save($scope.stage);
+    }, true);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,124 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Stages', function($rootScope) {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    //var storage = window.localStorage;
+    //storage.removeItem('stages');
+    //var sStages = storage.getItem('stages');
+    var stages;
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    //if (sStages === null || typeof sStages === 'undefined' || sStages == 'undefined') {
+    stages = [{
+            id: 0,
+            name: 'Stage 1',
+            length: new Number(30),
+            color: '#CDDC39'
+        },
+        {
+            id: 1,
+            name: 'Stage 2',
+            length: new Number(50),
+            color: '#FFB300'
+        },
+        {
+            id: 2,
+            name: 'Stage 3',
+            length: new Number(50),
+            color: '#4FC3F7'
+        },
+        {
+            id: 3,
+            name: 'Stage 4',
+            length: new Number(50),
+            color: '#FF7043'
         }
-      }
-      return null;
+
+    ];
+
+    saveStages();
+    //} else {
+    //    stages = JSON.parse(sStages);
+    //}
+
+    function getStages() {
+        //var jstring = storage.getItem('stages');
+        //stages = JSON.parse(jstring);
+        stages = $rootScope.stages;
     }
-  };
+
+    function saveStages() {
+        //storage.setItem('stages', JSON.stringify(stages));
+        $rootScope.stages = stages;
+    }
+
+    return {
+        all: function() {
+            getStages();
+            return stages;
+        },
+
+        count: function() {
+            getStages();
+            var total = new Number(0);
+            for (i = 0; i < stages.length; i++) {
+                total += new Number(stages[i].length);
+            }
+            return total;
+        },
+
+        majorTics: function() {
+            getStages();
+            // 0 20 40 60 80 100 120 140 160 180 200 220
+            var c = this.count();
+            var tics = "";
+            for (i = 0; i < c; i += 20) {
+                tics += i + " ";
+            }
+            tics += c;
+            return tics;
+        },
+
+        highlights: function() {
+            getStages();
+            var s = "";
+            var start = Number(0);
+            var comma = "";
+            for (i = 0; i < stages.length; i++) {
+                var e = parseInt(stages[i].length) + parseInt(start);
+                stages[i].start = start;
+                stages[i].end = e;
+                s += comma + start + " " + e + " " + stages[i].color;
+                start += parseInt(stages[i].length);
+                comma = ", ";
+            }
+            return s;
+        },
+
+        remove: function(stage) {
+            getStages();
+            stages.splice(chats.indexOf(stage), 1);
+            saveStages();
+        },
+
+        get: function(stageId) {
+            getStages();
+            for (var i = 0; i < stages.length; i++) {
+                if (stages[i].id === parseInt(stageId)) {
+                    return stages[i];
+                }
+            }
+            return null;
+        },
+
+        save: function(stage) {
+            for (var i = 0; i < stages.length; i++) {
+                if (stages[i].id === parseInt(stage.id)) {
+                    stages[i] = stage;
+                    saveStages();
+                    return;
+                }
+            }
+            return null;
+        }
+    };
 });
